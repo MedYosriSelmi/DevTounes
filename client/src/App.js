05f65345f44212ea189ecbefdd19781b348316e5
Med Yosri Selmi <60812060+MedYosriSelmi@users.noreply.github.com>
2022-10-30 21:20:23 +0100

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import './App.css';
 import Uploader from "./component/Uploader"
-
+import ReactGA from "react-ga4";
 
 import {
   BrowserRouter as Router,
@@ -9,6 +9,11 @@ import {
   
 } from "react-router-dom";
 function App() {
+  useEffect(()=>{
+    ReactGA.initialize("G-87Z37BJ7TL");
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  })
+
   return (
     <div className="App">
     <Routes>
